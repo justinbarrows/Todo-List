ac01f4b1c1c90454393ee430a7c0a35d8ea56dc1
justinbarrows <jbarrows1030@gmail.com>
2022-07-23 22:23:36 -0400

    added filter dropdown menu - you can select all, completed, or uncompleted, and it only shows what you select

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
+const filterOption = document.querySelector('.filter-todo')
 
 //Event Listeners
-todoButton.addEventListener('click', addTodo)
-todoList.addEventListener('click', deleteItem)
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteItem);
+filterOption.addEventListener('click', filterTodo);
 
 //Functions
 function addTodo(event) {
@@ -30,7 +32,7 @@ function addTodo(event) {
     trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
     //APPEND TO LIST
-    todoList.appendChild(todoDiv)
+    todoList.appendChild(todoDiv);
     //clear input value
     todoInput.value = '';
 }
@@ -52,4 +54,30 @@ function deleteItem(event) {
         const todo = item.parentElement;
         todo.classList.toggle('completed');
     }
+}
+
+function filterTodo(event) {
+    const todos = todoList.childNodes;
+    console.log(todos);
+    todos.forEach(function(todo){
+        switch(event.target.value){
+            case 'all':
+                todo.style.display = 'flex'
+                break;
+            case 'completed':
+                if(todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                } else{
+                    todo.style.display = 'none'
+                }
+                break;
+            case 'uncompleted':
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                } else{
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    })
 }
